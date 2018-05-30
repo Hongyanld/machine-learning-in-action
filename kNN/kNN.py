57a5cc90743869aea6820ecfa4fc3b0742bf8c9c
Hongyanld <hybao.ld@gmail.com>
2018-05-30 17:09:15 +0300

    a

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -3,9 +3,9 @@
 from numpy import *
 import operator
 
-def createDataset()
+def createDataset():
     group = arrary ([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
     labels = ['A','A','B','B']
     return group, labels
-    
+
 
