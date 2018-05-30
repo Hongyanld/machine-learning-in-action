09549bea41a86bc52815e6686a438a3b01483988
Hongyanld <hybao.ld@gmail.com>
2018-05-30 16:57:45 +0300

    change to kNN

---
+++ kNN.py
@@ -0,0 +1,11 @@
+#kmeans algorithm
+
+from numpy import *
+import operator
+
+def createDataset()
+    group = arrary ([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+    labels = ['A','A','B','B']
+    return group, labels
+    
+
